1f5e6a9f6aa8da63e7b03d087efd0525461fc3d4
hermiaTian <hermia1108@gmail.com>
2024-06-06 21:04:22 -0700

    add comment

--- Assets/Scripts/Enemy/EnemyState.cs
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class EnemyState : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.WriteLine("1111");   
     }
 
     // Update is called once per frame
